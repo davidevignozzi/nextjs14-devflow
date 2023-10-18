b629671a21957c2fefd1c66e4d54a6cf4fa7e386
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-18 18:42:02 +0200

    :iphone: Mobile navbar content

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Theme from './Theme';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -36,7 +37,9 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        MobileNavigationBar
+
+        {/* Mobile Nav */}
+        <MobileNav />
       </div>
     </nav>
   );
