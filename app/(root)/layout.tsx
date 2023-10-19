503d0abd176a2a1752f4217e2b8693fe6ea4d18c
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-19 10:58:35 +0200

    :lipstick: Right sidebar UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '@/components/shared/navbar/Navbar';
 import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
