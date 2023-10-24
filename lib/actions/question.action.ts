ad511990a42ab158b60e7e1482e551ef4bbb9a36
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-24 15:55:29 +0200

    :construction: Thinking in Backend

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,13 @@
+/**
+ *  all of server actions for the question model
+ *
+ */
+
+'use server';
+
+export async function createQuestion(params) {
+  // eslint-disable-next-line no-empty
+  try {
+    // connect to DB
+  } catch (error) {}
+}
