9ca5008551aa9bf0c981746944567e7133713cb2
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-11-02 15:29:51 +0100

    :card_file_box: Create answer action

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -1,6 +1,13 @@
 import { Schema } from 'mongoose';
 import { IUser } from '@/mongodb';
 
+export interface CreateAnswerParams {
+  content: string;
+  author: string; // User ID
+  question: string; // Question ID
+  path: string;
+}
+
 export interface GetQuestionsParams {
   page?: number;
   pageSize?: number;
