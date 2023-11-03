d07a66a1536d0694626f34f10575cad6d0fe09ff
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-11-03 11:13:28 +0100

    :lipstick: Votes UI

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from '@/components/shared/AllAnswers';
 import Metric from '@/components/shared/Metric';
 import ParseHTML from '@/components/shared/ParseHTML';
 import RenderTag from '@/components/shared/RenderTag';
+import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.actions';
 import { getUserByID } from '@/lib/actions/user.actions';
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
@@ -10,7 +11,7 @@ import { auth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const QuestionDetail = async ({ params, searchParams }) => {
+const QuestionDetail = async ({ params, searchParams }: any) => {
   // Access to mongo user
   const { userId: clerkId } = auth();
   let mongoUser;
@@ -40,7 +41,18 @@ const QuestionDetail = async ({ params, searchParams }) => {
             </p>
           </Link>
 
-          <div className="flex justify-end">{/* TODO */}Voting</div>
+          <div className="flex justify-end">
+            <Votes
+              type="question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasUpvoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasDownvoted={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
+          </div>
         </div>
 
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getTimestamp } from '@/lib/utils';
 import ParseHTML from './ParseHTML';
+import Votes from './Votes';
 
 interface Props {
   questionId: string;
@@ -62,7 +63,7 @@ const AllAnswers = async ({
                   </div>
                 </Link>
 
-                <div className="flex justify-end">Voting</div>
+                <div className="flex justify-end">{/* <Votes /> */}</div>
               </div>
             </div>
 
