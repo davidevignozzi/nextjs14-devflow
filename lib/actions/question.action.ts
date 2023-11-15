2d7c9f6150ee8edf09ded03f0db388f54c97c897
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-11-15 13:52:41 +0100

    :sparkles: Implement search functionality for the home

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -19,12 +19,27 @@ import User from '@/database/user.model';
 import Answer from '@/database/answer.model';
 import Interaction from '@/database/interaction.model';
 import { revalidatePath } from 'next/cache';
+import { FilterQuery } from 'mongoose';
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const questions = await Question.find({})
+    /**
+     * Search functionality
+     */
+    const { searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { title: { $regex: new RegExp(searchQuery, 'i') } },
+        { content: { $regex: new RegExp(searchQuery, 'i') } }
+      ];
+    }
+
+    const questions = await Question.find(query)
       .populate({
         path: 'tags',
         model: Tag
