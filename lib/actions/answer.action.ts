83fe58ee1545e091c68738baf9150bd24629a4f2
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-04 16:59:04 +0100

    :sparkles: Implement reputation points for answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -11,6 +11,7 @@ import {
 import Question from '@/database/question.model';
 import Interaction from '@/database/interaction.model';
 import { revalidatePath } from 'next/cache';
+import User from '@/database/user.model';
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -21,11 +22,20 @@ export async function createAnswer(params: CreateAnswerParams) {
     const newAnswer = await Answer.create({ content, author, question });
 
     // Add the answer to the question's answers array
-    await Question.findByIdAndUpdate(question, {
+    const questionObject = await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id }
     });
 
-    // TODO: Add interaction...
+    // Increment user reputation
+    await Interaction.create({
+      user: author,
+      action: 'answer',
+      question,
+      answer: newAnswer.id,
+      tags: questionObject.tags
+    });
+
+    await User.findByIdAndUpdate(author, { $inc: { reputation: 10 } });
 
     revalidatePath(path);
   } catch (error) {
@@ -116,6 +126,13 @@ export async function upvoteAnswer(params: AnswerVoteParams) {
     }
 
     // Increment author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasUpvoted ? -2 : 2 }
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasUpvoted ? -10 : 10 }
+    });
 
     revalidatePath(path);
   } catch (error) {
@@ -152,6 +169,13 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
     }
 
     // Decrement author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasDownvoted ? -2 : 2 }
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasDownvoted ? -10 : 10 }
+    });
 
     revalidatePath(path);
   } catch (error) {
