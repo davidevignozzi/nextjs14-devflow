0a7f793dafd27922e0bfe5cac348b438df2a60e6
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-11 13:26:10 +0100

    :construction: Setup AI answer feature

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -34,6 +34,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
   const { mode } = useTheme();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmittingAI, setIsSubmittingAI] = useState(false);
 
   const form = useForm<z.infer<typeof AnswerSchema>>({
     resolver: zodResolver(AnswerSchema),
@@ -74,6 +75,35 @@ const Answer = ({ question, questionId, authorId }: Props) => {
     }
   };
 
+  /**
+   * AI Feature
+   *
+   */
+  const generateAIAnswer = async () => {
+    if (!authorId) return;
+
+    setIsSubmittingAI(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ question })
+        }
+      );
+
+      const aiAnswer = await response.json();
+
+      alert(aiAnswer.reply);
+    } catch (error) {
+      console.error(`❌ ${error} ❌`);
+      throw error;
+    } finally {
+      setIsSubmittingAI(false);
+    }
+  };
+
   return (
     <div className="mt-8">
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -83,9 +113,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none"
-          onClick={() => {
-            console.log('AI button cliccked');
-          }}
+          onClick={generateAIAnswer}
         >
           <Image
             src="/assets/icons/stars.svg"
@@ -94,7 +122,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             height={12}
             className="object-contain"
           />
-          Generate an AI Answer
+          Generate AI Answer
         </Button>
       </div>
       <Form {...form}>
