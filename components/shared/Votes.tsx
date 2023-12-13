ebe4ac91a8bc4b378446748a5733dfd599399d58
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-13 16:29:44 +0100

    :sparkles: Create toasts

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -11,6 +11,7 @@ import { formatAndDivideNumber } from '@/lib/utils';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { toast } from '../ui/use-toast';
 
 interface Props {
   type: string;
@@ -42,11 +43,21 @@ const Votes = ({
       questionId: JSON.parse(itemId),
       path: pathname
     });
+
+    return toast({
+      title: `Question ${
+        !hasSaved ? 'saved in' : 'removed from'
+      } your collection`,
+      variant: !hasSaved ? 'default' : 'destructive'
+    });
   };
 
   const handleVote = async (action: string) => {
     if (!userId) {
-      return;
+      return toast({
+        title: 'Please log in',
+        description: 'You must be logged in to perform this action'
+      });
     }
 
     if (action === 'upvote') {
@@ -68,8 +79,10 @@ const Votes = ({
         });
       }
 
-      // TODO: show a toast
-      return;
+      return toast({
+        title: `Upvote ${!hasUpvoted ? 'Successful' : 'Removed'}`,
+        variant: !hasUpvoted ? 'default' : 'destructive'
+      });
     }
 
     if (action === 'downvote') {
@@ -91,7 +104,10 @@ const Votes = ({
         });
       }
 
-      // TODO: show a toast
+      return toast({
+        title: `Downvote ${!hasDownvoted ? 'Successful' : 'Removed'}`,
+        variant: !hasDownvoted ? 'default' : 'destructive'
+      });
     }
   };
 
