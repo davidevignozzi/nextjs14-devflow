87cecea97420512b97a8dcfc097403ae7a8160f5
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-14 10:49:43 +0100

    :bug: Home filters in dark mode

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -49,7 +49,7 @@ const HomeFilters = () => {
             onClick={() => handleTypeClick(filter.value)}
             className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
               active === filter.value
-                ? 'bg-primary-100 text-primary-500'
+                ? 'bg-primary-100 text-primary-500 dark:bg-dark-400'
                 : 'bg-light-800 text-light-500  hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400'
             }`}
           >
