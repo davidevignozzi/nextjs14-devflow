a8d173d3998780969f719468efd71c5f1ae42670
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-14 10:53:31 +0100

    :bug: Answer Alignment

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -43,42 +43,40 @@ const AllAnswers = async ({
             key={answer._id}
             className="light-border border-b py-10"
           >
-            <div className="flex items-center justify-between">
-              {/* Span Id Identifier */}
-              <div className="mb-8 flex w-full justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
-                <Link
-                  href={`/profile/${answer.author.clerkId}`}
-                  className="flex flex-1 items-start gap-1 sm:items-center"
-                >
-                  <Image
-                    src={answer.author.picture}
-                    alt="profile"
-                    width={18}
-                    height={18}
-                    className="rounded-full object-cover max-sm:mt-0.5"
-                  />
-                  <div className="flex flex-col sm:flex-row sm:items-center">
-                    <p className="body-semibold text-dark300_light700">
-                      {answer.author.name}
-                    </p>
+            {/* Span Id Identifier */}
+            <div className="mb-8 flex w-full justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+              <Link
+                href={`/profile/${answer.author.clerkId}`}
+                className="flex flex-1 items-start gap-1 sm:items-center"
+              >
+                <Image
+                  src={answer.author.picture}
+                  alt="profile"
+                  width={18}
+                  height={18}
+                  className="rounded-full object-cover max-sm:mt-0.5"
+                />
+                <div className="flex flex-col sm:flex-row sm:items-center">
+                  <p className="body-semibold text-dark300_light700">
+                    {answer.author.name}
+                  </p>
 
-                    <p className="small-regular text-light400_light500 ml-1 mt-0.5 line-clamp-1">
-                      - answered {getTimestamp(answer.createdAt)}
-                    </p>
-                  </div>
-                </Link>
-
-                <div className="flex justify-end">
-                  <Votes
-                    type="Answer"
-                    itemId={JSON.stringify(answer._id)}
-                    userId={JSON.stringify(userId)}
-                    upvotes={answer.upvotes.length}
-                    hasUpvoted={answer.upvotes.includes(userId)}
-                    downvotes={answer.downvotes.length}
-                    hasDownvoted={answer.downvotes.includes(userId)}
-                  />
+                  <p className="small-regular text-light400_light500 ml-1 mt-0.5 line-clamp-1">
+                    - answered {getTimestamp(answer.createdAt)}
+                  </p>
                 </div>
+              </Link>
+
+              <div className="flex justify-end">
+                <Votes
+                  type="Answer"
+                  itemId={JSON.stringify(answer._id)}
+                  userId={JSON.stringify(userId)}
+                  upvotes={answer.upvotes.length}
+                  hasUpvoted={answer.upvotes.includes(userId)}
+                  downvotes={answer.downvotes.length}
+                  hasDownvoted={answer.downvotes.includes(userId)}
+                />
               </div>
             </div>
 
