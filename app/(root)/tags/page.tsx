f7ee9ac7b0b6e3701e5af9502453be20214a15c2
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-18 15:55:58 +0100

    :iphone: Tag page error in responsive mode

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -41,7 +41,7 @@ const Tags = async ({ searchParams }: SearchParamsProps) => {
               href={`/tags/${tag._id}`}
               className="shadow-light100_darknone"
             >
-              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+              <article className="background-light900_dark200 light-border flex w-[210px] flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
                 <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
                   <p className="paragraph-semibold text-dark300_light900">
                     {tag.name}
