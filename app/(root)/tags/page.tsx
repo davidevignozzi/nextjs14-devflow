d09943d9c77ce99678a640e55fd4f4dd4397e5dd
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-12-18 16:38:31 +0100

    :adhesive_bandage: Fix => tag to uppercase in tag page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -43,7 +43,7 @@ const Tags = async ({ searchParams }: SearchParamsProps) => {
             >
               <article className="background-light900_dark200 light-border flex w-[210px] flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
                 <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
-                  <p className="paragraph-semibold text-dark300_light900">
+                  <p className="paragraph-semibold text-dark300_light900 uppercase">
                     {tag.name}
                   </p>
                 </div>
